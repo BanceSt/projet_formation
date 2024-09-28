7e0fcfeb34955ef8a5ca80217ac796da2102bc04
BanceSt <59577061+BanceSt@users.noreply.github.com>
2024-09-28 18:16:00 -0300

    table pivot tag story

--- app/Models/Tags.php
+++ app/Models/Tags.php
@@ -4,10 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tags extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        "name",
+        "description"
+    ];
+
+    public function stories(): BelongsToMany
+    {
+        return $this->belongsToMany(Story::class);
+    }
     public $timestamps = false;
 }
---
+++ database/migrations/2024_09_28_202244_create_story_tags_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('story_tags', function (Blueprint $table) {
+            $table->unsignedBigInteger("story_id");
+            $table->foreign("story_id")->references("id")->on("stories");
+
+            $table->unsignedBigInteger("tag_id");
+            $table->foreign("tag_id")->references("id")->on("tags");
+
+            $table->timestamps();
+            $table->primary(['story_id', 'tag_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('story_tags');
+    }
+};
