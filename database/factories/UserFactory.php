ffd8eda55afc4c527fea8dbfab2411e067d4151b
BanceSt <59577061+BanceSt@users.noreply.github.com>
2024-10-05 12:59:34 -0300

    user unique name

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'name' => fake()->unique()->name(),
             'email' => fake()->unique()->safeEmail(),
             'private_email' => fake()->boolean(),
             'description' => fake()->sentence(8),
