24a6a79007dd4fafaccc65bbb91f4e3516e7948f
BanceSt <59577061+BanceSt@users.noreply.github.com>
2024-10-28 18:24:03 -0300

    reduction size search barre

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,7 +16,7 @@
     <div class="h-full max-h-full min-h-screen">
         {{-- Navbar --}}
         <nav class="bg-gray-900 border-gray-200 ">
-            <div class="flex flex-wrap items-center justify-between max-w-full p-4">
+            <div class="flex flex-wrap items-center justify-between max-w-full p-2">
                 <a href="{{route('home')}}" class="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
                     <span class="self-center text-2xl font-semibold text-white whitespace-nowrap">Flowbite</span>
@@ -31,8 +31,13 @@
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                             </svg>
                         </div>
-                        <input type="search" id="default-search" class="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg ps-10 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
-                        <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                        <input type="search" id="default-search" class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg ps-10 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+                        <button type="submit" style="bottom : 0.0625rem;"
+                                class="text-white absolute end-0.5 bg-blue-700 hover:bg-blue-800
+                                focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2
+                                 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Search
+                        </button>
                     </div>
                 </form>
 
